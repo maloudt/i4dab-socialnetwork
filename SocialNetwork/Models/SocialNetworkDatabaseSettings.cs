5d614c7a7b9532b9ade42d35ddb5db9236edb4d3
Malou Troelsen <au543213@uni.au.dk>
2019-11-25 12:56:19 +0100

    DB opsætning + User model

---
+++ SocialNetwork/Models/SocialNetworkDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace SocialNetwork.Models
+{
+    public class SocialNetworkDatabaseSettings : ISocialNetworkDatabaseSettings
+    {
+        public string UsersCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface ISocialNetworkDatabaseSettings
+    {
+        string UsersCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
---
+++ SocialNetwork/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SocialNetwork.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
